3fd37aa9e77457629f15fe36865a7350cbbb0561
Ryan Braun <ryan@thelyricla.com>
2020-09-12 12:36:14 -0700

    built out schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO burger ( burger_name, devoured)
+VALUES (
+    "Cheesy Bacon Triple Heart Attack",
+    0
+    ),
+    (
+    "Goat Cheese Fried Egg English Burger",
+    0
+    ),
+    (
+    "Mushroom Arugula Burger",
+    0
+    );
